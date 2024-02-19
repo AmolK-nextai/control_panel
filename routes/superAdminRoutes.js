16f683b3d0938e19f94ee24c2f98f77ee30f5178
Amol Kharat <amol.k@nexaitech.com>
2024-02-19 11:05:41 +0530

    superadminapi testing done checked login , signup and  get all superadmins

--- routes/superAdminRoutes.js
+++ routes/superAdminRoutes.js
@@ -1,15 +1,16 @@
 const express =require('express');
-const router =express.router();
-const superAdminController = require('./controllers/superAdminController');
-const authMiddleware = require('../middleware/authMiddleware');
+const router = express.Router();
+const superAdminController = require('../controllers/superAdminController');
+
+const authMiddleware = require('../Middleware/authMiddleware');
 
 
 
 // Middleware to authenticate super admin
-router.use(authMiddleware.verifyTokenSuperAdmin);
-
+// router.use(authMiddleware.verifyTokenSuperAdmin);
+router.post('/login', superAdminController.loginSuperAdmin);
 router.get('/', superAdminController.getAllSuperAdmins);
 router.post('/signup', superAdminController.signupSuperAdmin);
-router.post('/login', superAdminController.loginSuperAdmin);
+
 
 module.exports = router;
